0a6daeb3e4833e6e1dbdfd13693e7308524fb45a
Mirko Klukas <mirko.klukas@gmail.com>
2026-03-15 14:51:59 +0100

    read load fix

--- src/psilia_edge/utils.py
+++ src/psilia_edge/utils.py
@@ -15,10 +15,14 @@ def write_yaml(path: Path, data: dict, parents=True, exist_ok=True) -> None:
     path.write_text(yaml.dump(dict(**data), default_flow_style=False))
 
 
-def load_yaml(path: Path) -> dict:
+def read_yaml(path: Path) -> dict:
     if not path.exists():
         return None
     return yaml.safe_load(path.read_text())
+
+
+load_yaml = read_yaml
+save_yaml = write_yaml
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
