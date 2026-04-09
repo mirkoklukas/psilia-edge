3a1dfc713fd54e0bd68544004e4735251eb0ef60
Mirko Klukas <mirko.klukas@gmail.com>
2026-04-09 16:53:49 +0200

    less topics

--- ros/psilia_runtime/psilia_runtime/nodes/diagnostics_node.py
+++ ros/psilia_runtime/psilia_runtime/nodes/diagnostics_node.py
@@ -27,10 +27,10 @@ MONITORED_TOPICS = [
     ("/psilia/stereo/image_raw",                Image),
     ("/psilia/stereo/image_rect",               Image),
     ("/psilia/stereo/depth",                    Image),
-    ("/psilia/preview/image/compressed",        CompressedImage),
-    ("/psilia/preview/depth/compressed",        CompressedImage),
-    ("/psilia/preview/rectified/compressed",    CompressedImage),
-    ("/psilia/preview/pointcloud",              PointCloud2),
+    # ("/psilia/preview/image/compressed",        CompressedImage),
+    # ("/psilia/preview/depth/compressed",        CompressedImage),
+    # ("/psilia/preview/rectified/compressed",    CompressedImage),
+    # ("/psilia/preview/pointcloud",              PointCloud2),
 ]
 
 
